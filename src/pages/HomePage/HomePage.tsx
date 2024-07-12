0d646237a7acfba97e7d92b3708e2dc27a3946e9
ILdar Nogmanov <nogmanov@gmail.com>
2024-07-12 22:59:18 +0300

    Fix

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -104,9 +104,9 @@ export function HomePage() {
 
   if (isLoading) return "Loading...";
 
-  if (isSendersLootbox) return <div>You can't open your lootboxes!</div>;
-
-  return (
+  if (isSendersLootbox) {
+    return <div>You can't open your lootboxes!</div>;
+  } else {
     <main className="flex min-h-screen flex-col items-center justify-center">
       <img src="./lootbox-closed.gif" alt="loading..." />
       <span className="text-center mt-50 p-5 pt-50 ">
@@ -117,6 +117,6 @@ export function HomePage() {
       <Link to="/tasks" className="bg-blue rounded p-2 px-10 text-white">
         Go!
       </Link>
-    </main>
-  );
+    </main>;
+  }
 }
