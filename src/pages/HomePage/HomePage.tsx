e46134819996855997619322caa1f1c76b60802d
ILdar Nogmanov <nogmanov@gmail.com>
2024-07-13 14:15:48 +0300

    1

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,85 +3,84 @@ import { initInitData } from "@telegram-apps/sdk";
 import { useEffect, useState } from "react";
 import { supabase } from "../../supabase";
 import { useNavigate } from "react-router-dom";
-import toast, { Toaster } from "react-hot-toast";
 
 export function HomePage() {
   const navigate = useNavigate();
   const initData = initInitData();
 
-  const [lootboxesCount, setLootboxesCount] = useState(1);
-  const [USDT, setUSDT] = useState(2);
-  const [LOOT, setLOOT] = useState(3);
+  // const [lootboxesCount, setLootboxesCount] = useState(1);
+  // const [USDT, setUSDT] = useState(2);
+  // const [LOOT, setLOOT] = useState(3);
 
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
 
-  // const [isSendersLootbox, setIsSendersLootbox] = useState(false);
+  const [isSendersLootbox, setIsSendersLootbox] = useState(false);
 
   // TODO avoid unnecceary calls if receiver_id is not NULL already
-  useEffect(() => {
-    const run = async () => {
-      // get startParam lootbox - parent and sender
-      try {
-        const [usersOpenedLootboxes, usersSendedLootboxes] = await Promise.all([
-          supabase
-            .from("lootboxes")
-            .select("balance")
-            .eq("receiver_id", initData?.user?.id as number),
-          supabase
-            .from("lootboxes")
-            .select("uuid")
-            .eq("sender_id", initData?.user?.id as number),
-        ]);
+  // useEffect(() => {
+  //   const run = async () => {
+  //     // get startParam lootbox - parent and sender
+  //     try {
+  //       const [usersOpenedLootboxes, usersSendedLootboxes] = await Promise.all([
+  //         supabase
+  //           .from("lootboxes")
+  //           .select("balance")
+  //           .eq("receiver_id", initData?.user?.id as number),
+  //         supabase
+  //           .from("lootboxes")
+  //           .select("uuid")
+  //           .eq("sender_id", initData?.user?.id as number),
+  //       ]);
 
-        // Проще проверять sender_id !== user.id
-        if (
-          usersSendedLootboxes.data
-            ?.map((i) => i.uuid)
-            .includes(initData?.startParam as string)
-        ) {
-          // setIsSendersLootbox(true);
-          setIsLoading(false);
-          return;
-        }
+  //       // Проще проверять sender_id !== user.id
+  //       if (
+  //         usersSendedLootboxes.data
+  //           ?.map((i) => i.uuid)
+  //           .includes(initData?.startParam as string)
+  //       ) {
+  //         setIsSendersLootbox(true);
+  //         setIsLoading(false);
+  //         return;
+  //       }
 
-        if (!usersOpenedLootboxes?.data?.length) {
-          setLootboxesCount(0);
-          setUSDT(0);
-          setLOOT(0);
-          setIsLoading(false);
-          return;
-        }
+  //       if (!usersOpenedLootboxes?.data?.length) {
+  //         setLootboxesCount(0);
+  //         setUSDT(0);
+  //         setLOOT(0);
+  //         setIsLoading(false);
+  //         return;
+  //       }
 
-        setLootboxesCount(usersOpenedLootboxes?.data.length);
+  //       setLootboxesCount(usersOpenedLootboxes?.data.length);
 
-        setUSDT(
-          usersOpenedLootboxes?.data
-            .map((i) => i.balance || 0) // Treat null balance as 0
-            .filter((i) => i < 11)
-            .reduce(
-              (accumulator, currentValue) => accumulator + currentValue,
-              0
-            ) // Provide a default value for reduce
-        );
+  //       setUSDT(
+  //         usersOpenedLootboxes?.data
+  //           .map((i) => i.balance || 0) // Treat null balance as 0
+  //           .filter((i) => i < 11)
+  //           .reduce(
+  //             (accumulator, currentValue) => accumulator + currentValue,
+  //             0
+  //           ) // Provide a default value for reduce
+  //       );
 
-        setLOOT(
-          usersOpenedLootboxes?.data
-            .map((i) => i.balance || 0) // Treat null balance as 0
-            .filter((i) => i > 40)
-            .reduce(
-              (accumulator, currentValue) => accumulator + currentValue,
-              0
-            ) // Provide a default value for reduce
-        );
+  //       setLOOT(
+  //         usersOpenedLootboxes?.data
+  //           .map((i) => i.balance || 0) // Treat null balance as 0
+  //           .filter((i) => i > 40)
+  //           .reduce(
+  //             (accumulator, currentValue) => accumulator + currentValue,
+  //             0
+  //           ) // Provide a default value for reduce
+  //       );
 
-        setIsLoading(false);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+  //       setIsLoading(false);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
 
-    run();
-  }, []);
+  //   run();
+  // }, []);
 
   useEffect(() => {
     const run = async () => {
@@ -101,6 +100,12 @@ export function HomePage() {
       // ничего не делаем, если пытаются вручную UUID в ссылке указать
       // и отгадывают реальный НЕоткрытый lootbox => go to next /tasks screen
       if (sender_id == null) return navigate("/tasks", { replace: true });
+
+      // Проще проверять sender_id !== user.id
+      if (sender_id === (initData?.user?.id as number)) {
+        setIsSendersLootbox(true);
+        return;
+      }
 
       await supabase
         .from("lootboxes")
@@ -127,30 +132,23 @@ export function HomePage() {
     run();
   }, []);
 
-  useEffect(() => {
-    toast("Hello World");
-  }, []);
-
-  const notify = () => toast("Here is your toast.");
-
   // ADD SPINNER HERE
 
-  if (isLoading) return <div>Loading...</div>;
-
-  // if (isSendersLootbox) return <div>You can't open your lootboxes!</div>;
+  // if (isLoading) return <div>Loading...</div>;
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <img src="./lootbox-closed.gif" alt="loading..." />
+      {isSendersLootbox && <div>You can't open your lootboxes!</div>}
+
+      {/* <img src="./lootbox-closed.gif" alt="loading..." />
       <span className="text-center mt-50 p-5 pt-50 ">
         {`You've already opened ${lootboxesCount} lootboxes and your balance is ${USDT} USDT and ${LOOT} LOOT.
         To open this box, you need to fulfill a task`}
-      </span>
+      </span> */}
+
       <Link to="/tasks" className="bg-blue rounded p-2 px-10 text-white">
         Go!
       </Link>
-      <button onClick={notify}>Make me a toast</button>
-      <Toaster />
     </main>
   );
 }
